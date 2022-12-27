bdfc7b88f7522d1f1b487f8d8d9786ba035614c5
Leah Tajon <leahtajon@gmail.com>
2022-12-27 17:00:57 +1000

    Update files

--- App.js
+++ App.js
@@ -1,40 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Video} from './Video';
-import {Menu} from './Menu';
+import { Video } from './Video';
+import { Menu } from './Menu';
 
 const VIDEOS = {
-    fast: 'https://content.codecademy.com/courses/React/react_video-fast.mp4',
-    slow: 'https://content.codecademy.com/courses/React/react_video-slow.mp4',
-    cute: 'https://content.codecademy.com/courses/React/react_video-cute.mp4',
-    eek: 'https://content.codecademy.com/courses/React/react_video-eek.mp4'
-}
+  fast: 'https://content.codecademy.com/courses/React/react_video-fast.mp4',
+  slow: 'https://content.codecademy.com/courses/React/react_video-slow.mp4',
+  cute: 'https://content.codecademy.com/courses/React/react_video-cute.mp4',
+  eek: 'https://content.codecademy.com/courses/React/react_video-eek.mp4'
+};
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {src: VIDEOS.fast};
-        this.chooseVideo = this.chooseVideo.bind(this);
-    }
+  constructor(props) {
+    super(props);
 
-    chooseVideo(newVideo) {
-        this.setState({
-            src: VIDEOS[newVideo]
-        });
-    }
+    this.state = { src: VIDEOS.fast };
+    this.chooseVideo = this.chooseVideo.bind(this);
+  }
+  
+  chooseVideo(newVideo) {
+    this.setState({
+      src: VIDEOS[newVideo]
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>Video Player</h1>
-                <Menu chooseVideo={this.chooseVideo}/>
-                <Video src={this.state.src}/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Video Player</h1>
+        <Menu chooseVideo={this.chooseVideo}/>
+        <Video src={this.state.src}/>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('app')
+  <App />, 
+  document.getElementById('app')
 );
